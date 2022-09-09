46c898cefd844b999f9e3df0f9cf473e280efb7c
root <root@ADHAM.localdomain>
2022-09-09 15:14:24 +0200

    raffle part 1

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,20 @@
+const { developmentChains } = require("../helper-hardhat-config")
+const BASE_FEE = ethers.utils.parseEther("0.25")
+const GAS_PRICE_LINK = 1e9
+
+module.exports = async function ({ getNameAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const args = [BASE_FEE, GAS_PRICE_LINK]
+    if (developmentChains.includes(network.name)) {
+        log("local network detected! deploying mocks...")
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+        log("Mocks Deployed!")
+        log("-----------------------------------------------")
+    }
+}
+module.exports.tag = ["all", "raffle"]
